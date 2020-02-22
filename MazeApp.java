a86959ec14ca32186212bd4c1cc38ebe9d48a67e
JoshGoA <josugoar@gmail.com>
2020-02-22 14:13:01 +0100

    Added getters and setters

--- MazeApp.java
+++ MazeApp.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
@@ -15,20 +14,19 @@ import javax.swing.SwingUtilities;
 
 public class MazeApp implements Runnable {
 
+    private static JFrame frame;
+
     private static final int ROWS = 20;
     private static final int COLS = 20;
+
     private static int[][] grid = new int[ROWS][COLS];
 
     public static void main(final String[] args) {
         // Invoke MazeApp asynchronously in event dispatch thread
         final MazeApp maze = new MazeApp();
         EventQueue.invokeLater(maze);
+        // TODO: Add CL methods HERE
         System.out.println("Running...");
-    }
-
-    @Override
-    public String toString() {
-        return String.format("MazeApp(shape: [%d, %d])", ROWS, COLS);
     }
 
     @Override
@@ -37,7 +35,8 @@ public class MazeApp implements Runnable {
     }
 
     public void initJFrame() {
-        final JFrame frame = new JFrame("MazeApp");
+        frame = new JFrame("MazeApp");
+        // Add JFrame components
         frame.add(initJPanel());
         // Set JFrame parameters
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,7 +44,9 @@ public class MazeApp implements Runnable {
         frame.setVisible(true);
         frame.pack();
         frame.setLocationRelativeTo(null);
-        final Component[] cells = ((JPanel) frame.getContentPane().getComponent(0)).getComponents();
+        // TEST
+        // final Component[] cells = ((JPanel)
+        // frame.getContentPane().getComponent(0)).getComponents();
     }
 
     private JPanel initJPanel() {
@@ -53,32 +54,41 @@ public class MazeApp implements Runnable {
         // Define individual Cell for each space in GridLayout
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLS; col++) {
-                final Cell cell = new Cell();
-                cell.addMouseListener(new elementListener(cell, new int[] { row, col }));
+                final Cell cell = new Cell(new int[] { row, col });
+                cell.addMouseListener(new elementListener(cell));
                 panel.add(cell);
             }
         }
         return panel;
     }
 
+    @Override
+    public String toString() {
+        return String.format("MazeApp(shape: [%d, %d])", ROWS, COLS);
+    }
+
+    public static JFrame getFrame() {
+        return frame;
+    }
+
+    public static int[][] getGrid() {
+        return grid;
+    }
+
     private static class Cell extends JPanel {
 
         private static final long serialVersionUID = 8465814529701152253L;
+
         private static final int CELL_SIZE = 20;
         private Color color = Color.WHITE;
 
-        public Cell() {
+        private final int[] seed;
+
+        public Cell(final int[] seed) {
+            this.seed = seed;
             // Set JPanel parameters
             this.setBorder(BorderFactory.createLineBorder(Color.GRAY));
             this.setPreferredSize(new Dimension(CELL_SIZE, CELL_SIZE));
-        }
-
-        public Color getColor() {
-            return color;
-        }
-
-        public void setColor(final Color color) {
-            this.color = color;
         }
 
         @Override
@@ -87,42 +97,65 @@ public class MazeApp implements Runnable {
             g.setColor(getColor());
             g.fillRect(0, 0, getWidth(), getHeight());
         }
+
+        @Override
+        public String toString() {
+            return String.format("Cell(seed: [%d, %d])", this.seed[0], this.seed[1]);
+        }
+
+        public Color getColor() {
+            return this.color;
+        }
+
+        public void setColor(final Color color) {
+            this.color = color;
+        }
+
+        public int[] getSeed() {
+            return this.seed;
+        }
+
     }
 
     private static class elementListener extends MouseAdapter {
 
         private final Cell panel;
-        private final int[] seed;
 
-        public elementListener(final Cell panel, final int[] seed) {
+        public elementListener(final Cell panel) {
             this.panel = panel;
-            this.seed = seed;
         }
 
         @Override
         public void mouseEntered(final MouseEvent event) {
             // Set BLACK if LeftMouseButton is pressed
             if (SwingUtilities.isLeftMouseButton(event)) {
-                MazeApp.grid[this.seed[0]][this.seed[1]] = 1;
+                MazeApp.grid[getPanel().getSeed()[0]][getPanel().getSeed()[1]] = 1;
                 // TEST (Remember to delete java.util.Arrays import)
-                for (int[] row : MazeApp.grid) {
+                for (final int[] row : MazeApp.grid) {
                     System.out.println(Arrays.toString(row));
                 }
+                System.out.println("");
                 //
                 panel.setColor(Color.BLACK);
                 panel.repaint();
                 // Set WHITE if LeftMouseButton is pressed
             } else if (SwingUtilities.isRightMouseButton(event)) {
-                MazeApp.grid[this.seed[0]][this.seed[1]] = 0;
+                MazeApp.grid[getPanel().getSeed()[0]][getPanel().getSeed()[1]] = 0;
                 // TEST (Remember to delete java.util.Arrays import)
-                for (int[] row : MazeApp.grid) {
+                for (final int[] row : MazeApp.grid) {
                     System.out.println(Arrays.toString(row));
                 }
+                System.out.println("");
                 //
                 panel.setColor(Color.WHITE);
                 panel.repaint();
             }
         }
+
+        public Cell getPanel() {
+            return this.panel;
+        }
+
     }
 
 }
